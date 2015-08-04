b43b844a3797efa08bdaf18836dd96f798a41b66
purtcer <purtcer@mail.ru>
2015-08-04 23:29:12 +0300

    Создание нового проекта службы контроля серверов 1С
    Создание проекта Maven и подключение к нему инструмента Cucumber

---
+++ src/main/java/mainTread.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Timer;
+
+public class mainTread {
+    static TrayIcon trayIcon;
+    static Integer timerPeriod = 60000;
+    static Timer timerServersTest = new Timer(timerPeriod, new TimerActivationEvents());
+
+    public static void main(String[] args) {
+
+        PopupMenu popup = new PopupMenu();
+
+        MenuItem authenticationItem = new MenuItem("Авторизация");
+        authenticationItem.addActionListener(new AuthenticationButtonClick());
+        popup.add(authenticationItem);
+
+        MenuItem notificationItem = new MenuItem("Настройки уведомлений");
+        notificationItem.addActionListener(new NotificationButtonClick());
+        popup.add(notificationItem);
+
+        MenuItem settingsItem = new MenuItem("Настройки серверов 1С");
+        settingsItem.addActionListener(new ServersSettingsButtonClick());
+        popup.add(settingsItem);
+
+        MenuItem exitItem = new MenuItem("Выход");
+        exitItem.addActionListener(new ExitButtonClick());
+        popup.add(exitItem);
+
+        SystemTray systemTray = SystemTray.getSystemTray();
+        Image image = getImageTray(true);
+
+        trayIcon = new TrayIcon(image);
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setPopupMenu(popup);
+        try {
+            systemTray.add(trayIcon);
+        } catch (AWTException ex) {
+            Logger.getLogger(mainTread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        timerServersTest.start();
+    }
+
+    private static Image getImageTray(Boolean isGood){
+        String imageName = (isGood)?("Circle_Green.png"):("Circle_Red.png");
+        URL imageUrl = mainTread.class.getResource("Images/" + imageName);
+        Image image = Toolkit.getDefaultToolkit().getImage(imageUrl);
+
+        return image;
+    }
+
+    static class ExitButtonClick implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    static class ServersSettingsButtonClick implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+//            ServersSettingsForm serververSettingsForm = new ServersSettingsForm();
+//            serververSettingsForm.show();
+        }
+    }
+
+    static class NotificationButtonClick implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            //EventsSettingsForm eventsSettingsForm = new EventsSettingsForm();
+            //eventsSettingsForm.setResizable(false);
+            //Dimension panelSize = new Dimension(400,215);
+            //eventsSettingsForm.setSize(panelSize);
+            //eventsSettingsForm.setVisible(true);
+        }
+    }
+
+    static class AuthenticationButtonClick implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+//            ServerAuthenticationForm serverAuthenticationSettingsForm = new ServerAuthenticationForm();
+//            serverAuthenticationSettingsForm.show();
+        }
+    }
+
+    static class TimerActivationEvents implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            timerServersTest.stop();
+            Image image = getImageTray(false);
+            trayIcon.setImage(image);
+            timerServersTest.start();
+        }
+    }
+}
